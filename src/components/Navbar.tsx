bd29096bb604571c3dbe12f1b9e26839fac3b212
Anurag <anuragyadav2516@gmail.com>
2026-01-14 00:50:53 +0530

    style: change m-auto to mx-auto

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { Sun } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <div className="max-w-250 w-full m-auto flex justify-between items-center py-4 text-white">
+    <div className="max-w-250 w-full mx-auto flex justify-between items-center py-4 text-white">
       <div className="font-bold text-2xl">Anurag.</div>
       <div className="font-semibold text-lg flex gap-6">
         <a href="">Home</a>
