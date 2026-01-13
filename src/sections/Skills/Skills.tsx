0244702cae73b7b2ac950e9637ded4938b0485bb
Anurag <anuragyadav2516@gmail.com>
2026-01-14 00:58:37 +0530

    chore: make skills folder

---
+++ src/sections/Skills/Skills.tsx
@@ -0,0 +1,19 @@
+
+
+const Skills = () => {
+  return (
+    <section className="max-w-250 w-full mx-auto ">
+      <div className="flex flex-col gap-2 ">
+        <h1 className="font-bold text-3xl ">Crafting Modern Web Interfaces</h1>
+        <p className="font-normal text-lg text-gray-400">
+          I focus on building modern, responsive front-end interfaces and
+          developing reliable web applications using current technologies.
+        </p>
+      </div>
+
+      
+    </section>
+  );
+};
+
+export default Skills;
