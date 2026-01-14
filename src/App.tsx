e782d964ee5e1e4578302c023e3fe7e127d58211
Anurag <anuragyadav2516@gmail.com>
2026-01-14 22:56:49 +0530

    feat: add Projects component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Hero from "./sections/Hero";
 import Navbar from "./components/Navbar";
+import Skills from "./sections/Skills/Skills";
+import Projects from "./sections/Projects/Projects";
 
 function App() {
   return (
     <div className="min-h-screen bg-background-color text-white overflow-hidden">
       <Navbar />
       <Hero />
+      <Skills />
+      <Projects />
     </div>
   );
 }
