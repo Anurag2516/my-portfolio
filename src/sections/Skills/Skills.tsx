a9c9610aa2f1106599d797830de51dd63c7920a0
Anurag <anuragyadav2516@gmail.com>
2026-01-14 18:47:46 +0530

    feat: add SkillCards component

--- src/sections/Skills/Skills.tsx
+++ src/sections/Skills/Skills.tsx
@@ -1,8 +1,9 @@
-
+import { skillCardsConifg } from "./SkillCards.data";
+import  SkillCards  from "./SkillCards";
 
 const Skills = () => {
   return (
-    <section className="max-w-250 w-full mx-auto ">
+    <section className="max-w-6xl w-full mx-auto mb-12">
       <div className="flex flex-col gap-2 ">
         <h1 className="font-bold text-3xl ">Crafting Modern Web Interfaces</h1>
         <p className="font-normal text-lg text-gray-400">
@@ -10,8 +11,11 @@ const Skills = () => {
           developing reliable web applications using current technologies.
         </p>
       </div>
-
-      
+      <div className="grid grid-cols-3 gap-6 pt-10">
+        {skillCardsConifg.map((card) => (
+          <SkillCards key={card.id} card={card} />
+        ))}
+      </div>
     </section>
   );
 };
