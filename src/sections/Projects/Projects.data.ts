082feb92a76c82530de033288054e1ef1165532b
Anurag <anuragyadav2516@gmail.com>
2026-01-16 01:02:12 +0530

    feat: add project data

---
+++ src/sections/Projects/Projects.data.ts
@@ -0,0 +1,41 @@
+import blogVerseHomeImg from "../../assets/blogVerseHome.png";
+import deployForgeImg from "../../assets/deployForge.png";
+import devDashboardImg from "../../assets/devDashboard.png";
+
+export interface ProjectConfig {
+  id: string;
+  featured: boolean;
+  image: string;
+  title: string;
+  description: string;
+  languages: string[];
+  bentoVertical?: boolean;
+}
+
+export const projectsData: ProjectConfig[] = [
+  {
+    id: "deployForge",
+    featured: true,
+    image: deployForgeImg,
+    title: "DeployForge",
+    description: "A fully responsive, production-grade SaaS landing page",
+    languages: ["React", "TailwindCSS", "Javascript", "Motion"],
+    bentoVertical: true,
+  },
+  {
+    id: "blogVerseBlog",
+    featured: true,
+    image: blogVerseHomeImg,
+    title: "BlogVerse",
+    description: "A simple and clean blog application",
+    languages: ["React", "TailwindCSS", "Javascript", "Lottie"],
+  },
+  {
+    id: "devDashboard",
+    featured: true,
+    image: devDashboardImg,
+    title: "DevDashboard",
+    description: "A modern, responsive multi-API dashboard",
+    languages: ["React", "TailwindCSS", "Javascript", "SwiperJS"],
+  },
+];
