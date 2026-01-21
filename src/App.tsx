a25fb9ea54e85cf269f229ea32d3fdbc89a2737d
Anurag <anuragyadav2516@gmail.com>
2026-01-21 17:55:47 +0530

    feat: add Contact and Footer components

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import Hero from "./sections/Hero";
 import Navbar from "./components/Navbar";
 import Skills from "./sections/Skills/Skills";
 import Projects from "./sections/Projects/Projects";
+import Contact from "./sections/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="min-h-screen bg-background-color text-white overflow-hidden">
+    <div className="min-h-screen bg-black/97 text-white overflow-hidden">
       <Navbar />
       <Hero />
       <Skills />
       <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
