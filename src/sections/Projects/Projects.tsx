c94826a01aeb7057f1b75c5219cdd3a15b8b7fe8
Anurag <anuragyadav2516@gmail.com>
2026-01-21 18:01:07 +0530

    feat: update structural layout and styling

--- src/sections/Projects/Projects.tsx
+++ src/sections/Projects/Projects.tsx
@@ -1,31 +1,47 @@
+import { FolderKanban } from "lucide-react";
 import { projectsData, type ProjectConfig } from "./Projects.data";
 import ProjectsCard from "./ProjectsCard";
 
 const Projects = () => {
   const featuredProjects: ProjectConfig[] = projectsData.filter(
-    (project) => project.featured === true
+    (project) => project.featured === true,
   );
 
   return (
-    <section className="max-w-6xl w-full mx-auto mt-16 sm:mt-20 pb-20 sm:pb-28 px-4 sm:px-6">
-      <div className="flex flex-col gap-2">
-        <h1 className="font-bold text-2xl sm:text-3xl">Featured Projects</h1>
-        <p className="font-normal text-base sm:text-lg text-gray-400">
-          A few projects showcasing my approach to modern front-end development.
-        </p>
-      </div>
+    <section className="my-20 sm:my-26 px-5 sm:px-10 md:px-14 lg:px-20 xl:px-0">
+      <div className="max-w-6xl w-full mx-auto flex flex-col gap-10">
+        <div className="flex flex-col gap-2">
+          <h1 className="font-bold text-2xl sm:text-3xl">Featured Projects</h1>
+          <p className="font-normal text-base sm:text-lg text-gray-400">
+            Selected projects showcasing my approach to front-end development.
+          </p>
+        </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-none md:grid-rows-2 gap-4 pt-8 sm:pt-12">
-        {featuredProjects.map((project) => (
-          <div
-            key={project.id}
-            className={
-              project.bentoVertical === true ? "md:row-span-2" : "md:row-span-1"
-            }
+        <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-none md:grid-rows-2 gap-4">
+          {featuredProjects.map((project) => (
+            <div
+              key={project.id}
+              className={
+                project.bentoVertical === true
+                  ? "md:row-span-2"
+                  : "md:row-span-1"
+              }
+            >
+              <ProjectsCard project={project} />
+            </div>
+          ))}
+        </div>
+        <div className="flex justify-end">
+          <button
+            type="button"
+            className="flex items-center gap-2 px-4 py-3 bg-btn-bg-dark hover:bg-btn-accent-dark border border-btn-accent-dark cursor-pointer hover:scale-105 transition-all duration-200 ease-out rounded-lg"
           >
-            <ProjectsCard project={project} />
-          </div>
-        ))}
+            <FolderKanban size={20} />
+            <span className="text-sm sm:text-base font-bold">
+              View All Projects
+            </span>
+          </button>
+        </div>
       </div>
     </section>
   );
