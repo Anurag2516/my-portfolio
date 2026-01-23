e935f7b10ddac7041457fcbfbb9a9713a3f6ca8b
Anurag <anuragyadav2516@gmail.com>
2026-01-23 21:44:59 +0530

    style: add dark mode background

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -4,12 +4,12 @@ import Socials from "../components/Socials";
 const Contact = () => {
   return (
     <section className="w-full mt-34 sm:mt-40 px-5 sm:px-10 md:px-14 lg:px-20 xl:px-0">
-      <div className="relative max-w-6xl mx-auto bg-gray-200 border border-black/20 dark:border-white/20 rounded-xl overflow-hidden px-6 sm:px-10 py-14">
-
-        <div className="absolute -top-35 -left-35 h-125 w-125 rounded-full dark:bg-purple-500/15 blur-[120px]" />{" "}
-        <div className="absolute top-30 left-40 h-125 w-125 rounded-full dark:bg-red-500/10 blur-[120px]" />{" "}
-        <div className="absolute bottom-0 right-0 h-100 w-100 rounded-full dark:bg-blue-500/10 blur-[120px]" />{" "}
-        <div className="absolute inset-0 dark:bg-linear-to-br from-purple-500/5 via-transparent to-black/40" />{" "}
+      <div className="relative max-w-6xl mx-auto bg-gray-200 dark:bg-transparent border border-black/20 dark:border-white/20 rounded-xl overflow-hidden px-6 sm:px-10 py-14">
+      
+        <div className="absolute -top-35 -left-35 h-125 w-125 rounded-full dark:bg-purple-500/15 dark:blur-[120px]" />
+        <div className="absolute top-30 left-40 h-125 w-125 rounded-full dark:bg-red-500/10 dark:blur-[120px]" />
+        <div className="absolute bottom-0 right-0 h-100 w-100 rounded-full dark:bg-blue-500/10 dark:blur-[120px]" />
+        <div className="absolute inset-0 dark:bg-linear-to-br dark:from-purple-500/5 dark:via-transparent dark:to-black/40" />
         <div className="absolute inset-0 dark:bg-[radial-gradient(ellipse_at_center,transparent_55%,rgba(0,0,0,0.6))]" />
 
         <div className="relative flex flex-col gap-8 justify-center items-center lg:flex-row lg:gap-0 lg:justify-between">
