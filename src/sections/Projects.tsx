167d49f19b6f61ccb2c3a015d37fc77c36e32064
Anurag <anuragyadav2516@gmail.com>
2026-07-03 12:27:44 +0530

    fix: adjust grid column layout

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -19,7 +19,7 @@ const Projects = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-none md:grid-rows-2 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-none gap-4">
           {featuredProjects.map((project) => (
             <div
               key={project.id}
